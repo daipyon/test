5bd457908230fa7c22302a73956e4b64b5abb420
daipyon <52702633+daipyon@users.noreply.github.com>
2020-07-25 14:03:56 +0900

    Update ThisMonth_Shuukei.ts

--- ThisMonth_Shuukei.ts
+++ ThisMonth_Shuukei.ts
@@ -29,8 +29,7 @@ const mySSID = process.env.My_SheetID2;
 // 今月のシートIDを取得
 let ThisMonthSheetID;
 // シート名
-// const SSName = [`News`, `公式`, `バラエティ`, `恋リア`, `Mリーグ`];
-const SSName = [`公式`];
+const SSName = [`News`, `公式`, `バラエティ`, `恋リア`, `Mリーグ`];
 // 現在のシート名
 let CurrentSSName;
 
